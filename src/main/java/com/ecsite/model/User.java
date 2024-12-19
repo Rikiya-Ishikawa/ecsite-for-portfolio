4925c6a8801890434d8f9ca5e3e64c9faf211560
Rikiya Ishikawa <flower.night.flower@gmail.com>
2024-12-20 01:13:58 +0900

    UserクラスとUserMapperの作成

---
+++ src/main/java/com/ecsite/model/User.java
@@ -0,0 +1,38 @@
+package com.ecsite;
+
+public class User {
+    private Long id;         // ユーザーID
+    private String username; // ユーザー名
+    private String password; // パスワード
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    // ゲッターとセッター
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
